9b427423738832eba76062482a776c0dbf101ab9
techtitansbvp-wq <techtitansbvp@gmail.com>
2026-02-03 00:36:10 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,15 +1,23 @@
-import { initializeApp } from
-"https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getAuth } from
-"https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+// js/firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
+// Your Firebase config copied from console
 const firebaseConfig = {
-  apiKey: "PASTE_YOUR_API_KEY",
-  authDomain: "PASTE_YOUR_AUTH_DOMAIN",
-  projectId: "PASTE_PROJECT_ID"
+  apiKey: "AIzaSyC-AXEmQjf8e8ly2UeDcJyZ3uxOTwinqi0",
+  authDomain: "campus-mitra-b5ce2.firebaseapp.com",
+  projectId: "campus-mitra-b5ce2",
+  storageBucket: "campus-mitra-b5ce2.firebasestorage.app",
+  messagingSenderId: "884366224201",
+  appId: "1:884366224201:web:56c93dffb9644c84a50898",
+  measurementId: "G-3ZFVV0XLYC"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// EXPORT so other files can use it
+// Auth + Firestore exports
 export const auth = getAuth(app);
+export const db = getFirestore(app);
+
